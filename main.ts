8760dc88ce97a7fda34ae2df6e880ce281000d25
mkmulvi30 <98337534+mkmulvi30@users.noreply.github.com>
2023-05-17 13:43:06 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,80 +5,87 @@ function PauseClear () {
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(ScoreCounter)
 })
-let bucket = 0
-let ScoreCounter = 0
-ScoreCounter = 0
-let lives = 3
-basic.forever(function () {
-    if (input.isGesture(Gesture.Shake)) {
-        bucket = randint(0, 25)
-        if (bucket <= 4) {
+input.onGesture(Gesture.Shake, function () {
+    bucket = randint(0, 25)
+    if (bucket <= 4) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . # # # .
+            # # # # #
+            `)
+        PauseClear()
+    } else if (bucket >= 5 && bucket <= 20) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+        PauseClear()
+    } else if (bucket == 0 || bucket == 21) {
+        basic.showString("Boom!")
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            # # # # #
+            . # # # .
+            . . . . .
+            `)
+        basic.showLeds(`
+            # . # . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . # . #
+            `)
+        PauseClear()
+        basic.showString("YOU LOSE")
+        lives += -3
+    } else if (bucket >= 22 && bucket < 25) {
+        basic.showLeds(`
+            # # . # #
+            . # # # .
+            . . # . .
+            . # # # .
+            # # . # #
+            `)
+        PauseClear()
+    } else if (bucket == 25) {
+        for (let index = 0; index < 2; index++) {
             basic.showLeds(`
-                . . . . .
-                . . . . .
-                . . # . .
-                . # # # .
-                # # # # #
-                `)
-            PauseClear()
-        } else if (bucket >= 5 && bucket <= 20) {
-            basic.showLeds(`
-                # . . . #
-                . # . # .
                 . . # . .
                 . . # . .
-                . . # . .
-                `)
-            PauseClear()
-        } else if (bucket == 0 || bucket == 21) {
-            basic.showString("Boom!")
-            basic.pause(100)
-            basic.showLeds(`
-                . . . . .
-                . # # # .
-                # # # # #
-                . # # # .
-                . . . . .
-                `)
-            PauseClear()
-            lives += -1
-        } else if (bucket >= 22 && bucket < 25) {
-            basic.showLeds(`
                 # # . # #
-                . # # # .
+                . . # . .
+                . . # . .
+                `)
+            basic.showLeds(`
+                . . . . .
                 . . # . .
                 . # # # .
-                # # . # #
+                . . # . .
+                . . . . .
+                `)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                . . . . .
                 `)
             PauseClear()
-        } else if (bucket == 25) {
-            for (let index = 0; index < 2; index++) {
-                basic.showLeds(`
-                    . . # . .
-                    . . # . .
-                    # # . # #
-                    . . # . .
-                    . . # . .
-                    `)
-                basic.showLeds(`
-                    . . . . .
-                    . . # . .
-                    . # # # .
-                    . . # . .
-                    . . . . .
-                    `)
-                basic.showLeds(`
-                    . . . . .
-                    . . . . .
-                    . . # . .
-                    . . . . .
-                    . . . . .
-                    `)
-                PauseClear()
-            }
-            ScoreCounter += 1
-        } else if (ScoreCounter == 3 || lives == 0) {
-            game.gameOver()
         }
+        ScoreCounter += 1
+    } else if (ScoreCounter == 3 || lives == 0) {
+        basic.showString("GAME OVER")
     }
 })
+let bucket = 0
+let lives = 0
+let ScoreCounter = 0
+ScoreCounter = 0
+lives = 3
